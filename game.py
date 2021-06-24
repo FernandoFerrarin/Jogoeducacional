aebe2ca697380110ed4193ddc488af0cee2f9cff
Fernando Ferrarin da Silva <fsgames01@hotmail.com>
2021-06-23 23:09:21 -0300

    adicionei o personagem e configurei sua movimentação

--- game.py
+++ game.py
@@ -10,6 +10,13 @@ fps = pygame.time.Clock()
 
 fundo = pygame.image.load('assets/fundo.jpg')
 
+#[Inicio] Personagens
+cria = pygame.image.load('assets/criança.png')
+criaPosciaoX = largura * 0.45
+criaPosicaoY = altura * 0.8
+
+#[Inicio] Movimentos
+movimentoX = 0
 #[Inicio] Cores
 Preto = (0, 0, 0)
 Branco = (255, 255, 255)
@@ -20,13 +27,25 @@ while True:
         if evento.type == pygame.QUIT:
             pygame.quit()
             quit()
-
-
-
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_a:
+                movimentoX = -10
+            elif evento.key == pygame.K_d:
+                movimentoX = 10
+        if evento.type == pygame.KEYUP:
+            movimentoX = 0
+    #[Fim] Verificação de interação
 
     display.fill(Branco)
     display.blit(fundo, (0, 0))
 
-    pygame.display.update()
+    #[Inicio] Movimentos da criança
+    criaPosciaoX = criaPosciaoX + movimentoX
+    if criaPosciaoX < 0:
+        criaPosciaoX = 0
+    elif criaPosciaoX > 745:
+        criaPosciaoX = 745
+    display.blit(cria, (criaPosciaoX, criaPosicaoY))
 
+    pygame.display.update()
     fps.tick(60)
